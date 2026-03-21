e3c68bdfaed8c4e8c5086919e392a9edf9c7694c
Marcos <marcosviniciuus0103@gmail.com>
2026-03-20 22:47:41 -0300

    Atividade Musica

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("Hino do Brasil");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Ouviram do Ipiranga as margens plácidas");
+Console.WriteLine("De um povo heróico o brado retumbante");
