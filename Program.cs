91c6f476b51914662ce72006d9f5d5b5b99dbe5d
Marcos <marcosviniciuus0103@gmail.com>
2026-03-26 21:59:08 -0300

    conclusão atividade musica

--- Program.cs
+++ Program.cs
@@ -6,5 +6,10 @@ Console.WriteLine();
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Ouviram do Ipiranga as margens plácidas");
-Console.WriteLine("De um povo heróico o brado retumbante");
-Console.WriteLine
+Console.WriteLine("De um povo heróico o brado retumbante"); 
+Console.WriteLine("E o Sol da liberdade, em raios fúlgidos");
+Console.WriteLine("Brilhou no céu da pátria nesse instante");
+Console.WriteLine("Se o penhor dessa igualdade");
+Console.WriteLine("Conseguimos conquistar com braço forte");
+Console.WriteLine("Em teu seio, ó liberdade");
+Console.WriteLine("Desafia o nosso peito a própria morte");
